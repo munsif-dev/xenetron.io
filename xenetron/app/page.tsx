711c375ecd8843527095bf08833e89136645417c
Ahamed Munsif <113419042+munsif-dev@users.noreply.github.com>
2025-04-13 11:58:59 +0530

    testimonial section is not updated for now

--- xenetron/app/page.tsx
+++ xenetron/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <ServicesSection />
       <ProductsSection />
       <CoursesSection />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <FaqSection />
       <ContactSection />
     </>
